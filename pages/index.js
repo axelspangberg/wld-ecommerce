f5f302900c2b7fc290d6c3ce4a5c736e8c205d03
Axel Spångberg <axspangberg@gmail.com>
2021-02-13 12:33:02 +0100

    fixed initial commit

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,48 @@
 import Head from 'next/head';
+
 import Header from '@components/Header';
 import Footer from '@components/Footer';
+import { fetchEntries } from 'utils/contentfulPosts';
+import Post from '@components/Post';
 
-export default function Home() {
+export default function Home({ posts }) {
   return (
     <div className='container'>
       <Head>
-        <title>Next.js Starter!</title>
+        <title>WLD Ecommerce</title>
         <link rel='icon' href='/favicon.ico' />
       </Head>
 
       <main>
-        <Header title='Welcome to my app!' />
-        <p className='description'>
-          Hello! Get started by editing <code>pages/index.js</code>
-        </p>
+        <Header title='WLD Ecommerce' />
+        <div className='posts'>
+          {posts.map((p) => {
+            return (
+              <Post
+                key={p.date}
+                date={p.date}
+                image={p.image.fields}
+                title={p.title}
+              />
+            );
+          })}
+        </div>
       </main>
 
       <Footer />
     </div>
   );
+}
+
+export async function getStaticProps() {
+  const res = await fetchEntries();
+  const posts = await res.map((p) => {
+    return p.fields;
+  });
+
+  return {
+    props: {
+      posts,
+    },
+  };
 }
